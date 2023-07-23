97404906c4994036c0afe963033f4bdc8b42e2cb
polochinoc <paul.charles@tutanota.com>
2023-07-23 16:14:06 +0200

    Added PyLint badge to README & applied Pylint to config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,19 @@
+"""
+Provides functions parsing the YAML Configuration file to retrieve parameters.
+"""
+
 from yaml import safe_load
 
-CONFIG_YAML: str = 'config.yaml'
+CONFIG_FILE_PATH: str = './config.yaml'
 
 
 def get_dataset_url() -> str:
+    """
+    Build the dataset URL location from the configuration.
+    :return: The dataset URL as a String.
+    """
     dataset_url: str = ""
-    with open(CONFIG_YAML, 'r') as stream:
+    with open(CONFIG_FILE_PATH, mode='rt', encoding='utf-8') as stream:
         yaml_config: dict = safe_load(stream)
         dataset_url += yaml_config['base_url']
 
@@ -18,8 +26,12 @@ def get_dataset_url() -> str:
 
 
 def get_start_year() -> int:
+    """
+    Retrieve the year the data should start at.
+    :return: A year as an Integer.
+    """
     start_year: int = 0
-    with open(CONFIG_YAML, 'r') as stream:
+    with open(CONFIG_FILE_PATH, mode='rt', encoding='utf-8') as stream:
         yaml_config: dict = safe_load(stream)
         start_year += yaml_config['data']['start_year']
 
@@ -27,8 +39,12 @@ def get_start_year() -> int:
 
 
 def get_round_precision() -> int:
+    """
+    The decimal precision of Rainfall values.
+    :return: A rounding precision as an Integer.
+    """
     rounding_precision: int = 0
-    with open(CONFIG_YAML, 'r') as stream:
+    with open(CONFIG_FILE_PATH, mode='rt', encoding='utf-8') as stream:
         yaml_config: dict = safe_load(stream)
         rounding_precision += yaml_config['data']['round_precision']
 
