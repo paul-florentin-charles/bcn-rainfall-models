6d9eb06909adc4f59fbd24320d9812052a939806
polochinoc <paul.charles@tutanota.com>
2025-02-11 22:54:53 +0100

    Lower plot font on mobile (code is a bit disgusting, I'll need to learn typescript and factorize this shit)

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -5,11 +5,27 @@ const config = {
     scrollZoom: true,
 };
 
+const graph_div_id_to_graph_json = {
+    chart_summer_rainfall: graphs_summer_rainfall,
+    chart_averages: graphs_averages,
+    chart_linreg: graphs_linreg,
+    chart_relative_distance_to_normal: graphs_relative_distance_to_normal,
+};
+
 ['load', 'resize'].forEach(event => window.addEventListener(event, function () {
-    Plotly.newPlot('chart_summer_rainfall', graphs_summer_rainfall, {}, config);
-    Plotly.newPlot('chart_averages', graphs_averages, {}, config);
-    Plotly.newPlot('chart_linreg', graphs_linreg, {}, config);
-    Plotly.newPlot('chart_relative_distance_to_normal', graphs_relative_distance_to_normal, {}, config);
+    if (window.screen.width < 768) {
+        Object.values(graph_div_id_to_graph_json).forEach((graph_json) => {
+            graph_json.layout.font.size = 9;
+        })
+    } else {
+        Object.values(graph_div_id_to_graph_json).forEach((graph_json) => {
+            graph_json.layout.font.size = 11;
+        })
+    }
+
+    Object.entries(graph_div_id_to_graph_json).forEach(([graph_div_id, graph_json]) => {
+        Plotly.newPlot(graph_div_id, graph_json, {}, config);
+    })
 }))
 
 /** Enable drop-down menus when device is tablet or phone **/
